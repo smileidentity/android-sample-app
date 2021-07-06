a6bc7a0ef9ba16ec8e48ff6dffba9ae1a583e1a0
Arlon Mukeba <arlon.mukeba@gmail.com>
2021-07-06 14:10:33 +0200

    Added consent to job capture process

--- app/src/main/java/com/demo/smileid/sid_sdk/BaseSIDActivity.java
+++ app/src/main/java/com/demo/smileid/sid_sdk/BaseSIDActivity.java
@@ -1,25 +1,30 @@
 package com.demo.smileid.sid_sdk;
 
 import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.demo.smileid.sid_sdk.geoloc.SIDGeoInfos;
+import com.smileidentity.libsmileid.core.consent.ConsentCategory;
+import com.smileidentity.libsmileid.core.consent.ConsentDialog;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class BaseSIDActivity extends AppCompatActivity {
+public class BaseSIDActivity extends AppCompatActivity implements ConsentDialog.DlgListener {
     protected boolean mUseMultipleEnroll = false, mUseOffLineAuth = false;
     protected int jobType = -1;
     private static final int PERMISSION_ALL = 1;
+    private Intent mCurrentIntent = null;
 
     protected String[] PERMISSIONS = {
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -40,16 +45,16 @@ public class BaseSIDActivity extends AppCompatActivity {
     }
 
     protected void startSelfieCapture(boolean isEnrollMode, boolean hasId, boolean use258, boolean reenroll, boolean hasNoIdCard) {
-        Intent intent = new Intent(this, SIDSelfieActivity.class);
-        intent.putExtra(SIDStringExtras.EXTRA_ENROLL_MODE, isEnrollMode);
-        intent.putExtra(SIDStringExtras.EXTRA_HAS_ID, hasId);
-        intent.putExtra(SIDStringExtras.EXTRA_USE_258, use258);
-        intent.putExtra(SIDStringExtras.EXTRA_REENROLL, reenroll);
-        intent.putExtra(SIDStringExtras.EXTRA_ENROLL_TYPE, jobType);
-        intent.putExtra(SIDStringExtras.EXTRA_MULTIPLE_ENROLL, mUseMultipleEnroll);
-        intent.putExtra(SIDStringExtras.EXTRA_HAS_NO_ID_CARD, hasNoIdCard);
-        intent.putExtra(SIDStringExtras.EXTRA_TAG_OFFLINE_AUTH, mUseOffLineAuth);
-        coreStartSelfieCapture(intent);
+        mCurrentIntent = new Intent(this, SIDSelfieActivity.class);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_ENROLL_MODE, isEnrollMode);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_HAS_ID, hasId);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_USE_258, use258);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_REENROLL, reenroll);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_ENROLL_TYPE, jobType);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_MULTIPLE_ENROLL, mUseMultipleEnroll);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_HAS_NO_ID_CARD, hasNoIdCard);
+        mCurrentIntent.putExtra(SIDStringExtras.EXTRA_TAG_OFFLINE_AUTH, mUseOffLineAuth);
+        requestUserConsent();
     }
 
     protected void startSelfieCapture(boolean isEnrollMode) {
@@ -82,5 +87,34 @@ public class BaseSIDActivity extends AppCompatActivity {
             }
         }
         return true;
+    }
+
+    private void requestUserConsent() {
+        //To be replaced by a partner-set values as returned by the backend
+        ArrayList<ConsentCategory> categories = new ArrayList<ConsentCategory>() {
+            {
+                add(new ConsentCategory(R.drawable.ic_personal_details, getString(R.string.user_consent_cat_lbl_1), getString(R.string.user_consent_tooltip_lbl_1), getString(R.string.user_consent_cat_tooltip_1)));
+                add(new ConsentCategory(R.drawable.ic_contact_info, getString(R.string.user_consent_cat_lbl_2), getString(R.string.user_consent_tooltip_lbl_2), getString(R.string.user_consent_cat_tooltip_2)));
+                add(new ConsentCategory(R.drawable.ic_doc_info, getString(R.string.user_consent_cat_lbl_3), getString(R.string.user_consent_tooltip_lbl_3), getString(R.string.user_consent_cat_tooltip_3)));
+            }
+        };
+
+        //Partner's name shouldn't be hardcoded
+        new ConsentDialog.Builder().setPartnerLogo(R.drawable.ic_bvn).setPartnerName("Piggyvest")
+            .setInfoCats(categories).setListener(this).build(this).showDialog();
+    }
+
+    @Override
+    public void decline() {
+        mCurrentIntent = null;
+        //A more appropriate message should be provided
+        String message = "You need to provide consent in order to proceed";
+        Toast.makeText(this, message, Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void approve() {
+        coreStartSelfieCapture(mCurrentIntent);
+        mCurrentIntent = null;
     }
 }
