c98170f73c3171bcc1f64718268f5826da985736
Arlon Mukeba <arlon.mukeba@gmail.com>
2022-06-30 17:34:47 +0200

    Design refining, code cleanup, screen flow

--- app/src/main/java/com/demo/smileid/sid_sdk/HomeFragment.java
+++ app/src/main/java/com/demo/smileid/sid_sdk/HomeFragment.java
@@ -9,6 +9,12 @@ public class HomeFragment extends BaseFragment {
 
     @Override
     void setupViews() {
-
+        if (mActionListener == null) return;
+        getView().findViewById(R.id.tvResBtn).setOnClickListener(v -> mActionListener.move2Tab(1));
+        getView().findViewById(R.id.cpbBasicKYC).setOnClickListener(v -> mActionListener.doBasicKYC());
+        getView().findViewById(R.id.cpbEnhancedKYC).setOnClickListener(v -> mActionListener.doEnhancedKYC());
+        getView().findViewById(R.id.cpbBiometricKYC).setOnClickListener(v -> mActionListener.doBiometricKYC());
+        getView().findViewById(R.id.cpbDocV).setOnClickListener(v -> mActionListener.performDocV());
+        getView().findViewById(R.id.cpbSmartSelfie).setOnClickListener(v -> mActionListener.doSmartSelfieAuth());
     }
 }
--- app/src/main/java/com/demo/smileid/sid_sdk/SIDHomeActivity.java
+++ app/src/main/java/com/demo/smileid/sid_sdk/SIDHomeActivity.java
@@ -1,22 +1,30 @@
 package com.demo.smileid.sid_sdk;
 
+import static com.demo.smileid.sid_sdk.SIDStringExtras.EXTRA_TAG_PREFERENCES_AUTH_TAGS;
+import static com.demo.smileid.sid_sdk.SIDStringExtras.SHARED_PREF_USER_ID;
+
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.text.TextUtils;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.TextView;
-
+import android.widget.Toast;
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentPagerAdapter;
-
 import com.google.android.material.tabs.TabLayout;
-
 import java.util.ArrayList;
 
-public class SIDHomeActivity extends AppCompatActivity {
+public class SIDHomeActivity extends BaseSIDActivity implements BaseFragment.TabActionListener {
+
+    private StaticPager mStaticPager = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,11 +39,97 @@ public class SIDHomeActivity extends AppCompatActivity {
             R.string.lbl_version_number_2), BuildConfig.VERSION_NAME));
 
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tlMenu);
-        StaticPager pager = (StaticPager) findViewById(R.id.spScreens);
-        pager.setAdapter(new ScreensPagerAdapter(getSupportFragmentManager()));
-        pager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
-        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(pager));
-        pager.setOffscreenPageLimit(3);
+        mStaticPager = (StaticPager) findViewById(R.id.spScreens);
+        mStaticPager.setAdapter(new ScreensPagerAdapter(getSupportFragmentManager()));
+        mStaticPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mStaticPager));
+        mStaticPager.setOffscreenPageLimit(3);
+    }
+
+    private void resetJob() {
+        jobType = -1;
+        mUseMultipleEnroll = false;
+        mUseOffLineAuth = false;
+    }
+
+    @Override
+    public void doBasicKYC() {
+        resetJob();
+        jobType = 4;
+        startSelfieCapture(true, false);
+    }
+
+    @Override
+    public void doEnhancedKYC() {
+        resetJob();
+        mConsentRequired = true;
+        jobType = 1;
+        startSelfieCapture(true, true, false, false, true);
+    }
+
+    @Override
+    public void doBiometricKYC() {
+        resetJob();
+        jobType = 1;
+        startSelfieCapture(true);
+    }
+
+    @Override
+    public void performDocV() {
+        mConsentRequired = true;
+        resetJob();
+        jobType = 6;
+        startSelfieCapture(true);
+    }
+
+    @Override
+    public void doSmartSelfieAuth() {
+        resetJob();
+        jobType = 2;
+
+        if (!hasSavedUser()) {
+            enrolFirstDialog();
+            return;
+        }
+
+        startSelfieCapture(false);
+    }
+
+    private void enrolFirstDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You have to enrol (register) first before you can authenticate");
+        builder.setCancelable(false);
+
+        builder.setPositiveButton(
+                "Register (Enroll)",
+                (dialog, id) -> startSelfieCapture(true, false));
+
+        builder.setNegativeButton(
+                "Cancel",
+                (dialog, id) -> dialog.cancel());
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    private boolean hasSavedUser() {
+        SharedPreferences sharedPreferences = getSharedPreferences(getPackageName(), Context.MODE_PRIVATE);
+        String tags = sharedPreferences.getString(SHARED_PREF_USER_ID, null);
+        return !TextUtils.isEmpty(tags);
+    }
+
+    @Override
+    public void openUrl(String url) {
+        if (url == null || url.isEmpty()) {
+            Toast.makeText(this, "URL not provided", Toast.LENGTH_LONG).show();
+            return;
+        }
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+    }
+
+    @Override
+    public void move2Tab(int position) {
+        mStaticPager.setCurrentItem(position, true);
     }
 
     class ScreensPagerAdapter extends FragmentPagerAdapter {
@@ -70,6 +164,7 @@ public class SIDHomeActivity extends AppCompatActivity {
         @NonNull
         public Object instantiateItem(@NonNull ViewGroup container, int position) {
             BaseFragment createdFragment = (BaseFragment) super.instantiateItem(container, position);
+            createdFragment.setListener(SIDHomeActivity.this);
             mFragments.add(createdFragment);
             return createdFragment;
         }
