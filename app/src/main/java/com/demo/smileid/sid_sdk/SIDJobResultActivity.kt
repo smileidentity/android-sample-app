c830e9d86e72d6882e4a76a338b7b77850702dd1
Juma Allan <allanjuma@gmail.com>
2025-03-19 15:01:09 +0200

    add removed timeout

--- app/src/main/java/com/demo/smileid/sid_sdk/SIDJobResultActivity.kt
+++ app/src/main/java/com/demo/smileid/sid_sdk/SIDJobResultActivity.kt
@@ -105,7 +105,7 @@ class SIDJobResultActivity : BaseSIDActivity(),
         mSIDNetworkRequest.setOnAuthenticatedListener(this)
         mSIDNetworkRequest.setOnDocVerificationListener(this)
         mSIDNetworkRequest.setOnIDValidationListener(this)
-//        mSIDNetworkRequest.setJobStatusPollingTimeoutMillis(120_000)
+        mSIDNetworkRequest.setJobStatusPollingTimeoutMillis(120_000)
     }
 
     fun uploadNow(view: View?) {
