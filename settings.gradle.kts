1d68d8544b5b1f2f90c92c0c81941e13584acdac
Juma Allan <allanjuma@gmail.com>
2025-10-09 12:26:10 +0200

    rename app to sample

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,26 @@
+@file:Suppress("UnstableApiUsage")
+
 pluginManagement {
-    repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
         google()
         mavenCentral()
+        // gradlePluginPortal is last for faster builds
+        // see: https://developer.android.com/studio/build/optimize-your-build#gradle_plugin_portal
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode = RepositoriesMode.FAIL_ON_PROJECT_REPOS
+    repositories {
+        google()
+        mavenCentral()
+        maven(url = "https://central.sonatype.com/repository/maven-snapshots/")
     }
 }
 
 rootProject.name = "SmileID"
-include(":app")
- 
+
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
+include("sample")
+rootProject.children.forEach { it.buildFileName = "${it.name}.gradle.kts" }
