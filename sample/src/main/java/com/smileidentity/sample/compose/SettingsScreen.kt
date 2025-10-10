137108f6cbf9a7744424368b3a750a4f8d082cc0
Juma Allan <allanjuma@gmail.com>
2025-10-10 21:06:03 +0200

    lint fixes

--- sample/src/main/java/com/smileidentity/sample/compose/SettingsScreen.kt
+++ sample/src/main/java/com/smileidentity/sample/compose/SettingsScreen.kt
@@ -1,17 +1,13 @@
 package com.smileidentity.sample.compose
 
-import android.content.Intent
 import android.widget.Toast
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.text.selection.SelectionContainer
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.Icons.AutoMirrored
 import androidx.compose.material.icons.automirrored.filled.ArrowForward
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.ListItem
@@ -25,7 +21,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.chuckerteam.chucker.api.ChuckerInterceptor
